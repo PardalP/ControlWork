3c5e62d324bb99ef48512f50868dcee919bc0261
Pardal <pardal2608@gmail.com>
2023-06-20 14:54:48 +0600

    Code

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+static string[] FilterStrings(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] result = new string[count];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[index] = array[i];
+            index++;
+        }
+    }
+
+    return result;
+}
+
+
+Console.WriteLine("Введите массив строк через запятую:");
+string[] array = Console.ReadLine().Split(',');
+
+string[] filteredArray = FilterStrings(array);
+
+Console.WriteLine("Результат:");
+foreach (string str in filteredArray)
+{
+    Console.WriteLine(str);
+}
